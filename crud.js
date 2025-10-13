74a8a3c483c44f44ad64c03b32293f906440ba3c
AMIN TROUDI <troudima69@gmail.com>
2025-10-13 23:55:19 +0100

    2eme commit

--- crud.js
+++ crud.js
@@ -18,6 +18,8 @@ let count=document.getElementById('count');
 let category=document.getElementById('category');
 let submit=document.getElementById('submit');
 //console.log(title,price,taxes,ads,discount,total,count,category,submit);
+let moode='create';
+let temp;
 
 //get total
 function gettotal(){
@@ -44,6 +46,7 @@ function create(){
     let newcr={
         title:title.value,
         price:price.value,
+        taxes:price.value,
         ads:ads.value,
         discount:discount.value,
         total:total.innerHTML,
@@ -51,7 +54,99 @@ function create(){
         category:category.value,
         
     }
-    atapro.push(newcr);
-    localStorage.setItem('product' , JSON.stringify(atapro))
-    console.log(atapro);
+    if(moode==='create'){
+        if(newcr.count>1){
+            for(let i=0;i<newcr.count;i++){
+                atapro.push(newcr);
+            }
+        }else{
+            atapro.push(newcr);
+        }
+    }else{
+        atapro[temp]=newcr;
+        moode='create';
+        submit.innerHTML='Create';
+        count.style.display='block';
+    }
+
+    
+    localStorage.setItem('product' , JSON.stringify(atapro));
+    clear();
+    stockdata();
+    
+    
+
+    //console.log(atapro);
+}
+function clear(){
+    title.value='';
+    price.value='';
+    taxes.value='';
+    ads.value='';
+    discount.value='';
+    total.innerHTML='';
+    count.value='';
+    category.value='';
+}
+function stockdata()
+{
+    gettotal()
+    let table='';
+    
+    for(let i=0;i<atapro.length;i++){
+        table +=`
+    
+        <tr>
+            <td>${i}</td>
+            <td>${atapro[i].title}</td>
+            <td>${atapro[i].price}</td>
+            <td>${atapro[i].taxes}</td>
+            <td>${atapro[i].ads}</td>
+            <td>${atapro[i].discount}</td>
+            <td>${atapro[i].total}</td>
+            <td>${atapro[i].category}</td>
+            <td><button id="update" onclick="updatedata(${i})">UPDATE</button></td>
+            <td><button id="delete" onclick="deletedata(${i})">DELETE</button></td>
+        </tr>
+        `;
+        let btndelete=document.getElementById('deleteall');
+
+        if(atapro.length>0){
+            btndelete.innerHTML=`<button onclick="clearall()">DELETE ALL ${atapro.length}</button>`
+
+        }
+        
+        //console.log(table);
+    }
+    document.getElementById('tbody').innerHTML=table ;
+}
+stockdata();
+
+function deletedata(i){
+    atapro.splice(i,1);
+    localStorage.product=JSON.stringify(atapro);
+    stockdata()
+}
+function clearall(){
+    localStorage.clear();
+    atapro.splice(0);
+    stockdata();
+}
+function updatedata(i){
+    //console.log(i);
+    title.value=atapro[i].title ;
+    price.value=atapro[i].price ;
+    taxes.value=atapro[i].taxes ;
+    ads.value=atapro[i].ads ;
+    discount.value=atapro[i].discount ;
+    gettotal();
+    count.style.display='none';
+    category.value=atapro[i].category ;
+    submit.innerHTML='Update';
+    moode='Update';
+    temp=i;
+    scroll({
+        top:0,
+        behavior:"smooth",
+    })
 }
